baa140104278778a8883356a1cb1e10ac2e3e51b
지원 <cheerfulboys@naver.com>
2025-02-28 01:55:36 +0900

    feat: 댓글 기능 추가

--- js/index.js
+++ js/index.js
@@ -16,4 +16,30 @@ console.log(
   "color: #d81b60; font-size: 16px; font-weight: bold;"
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+document.addEventListener("DOMContentLoaded", function () {
+  const commentInput = document.querySelector();
+  const submitButton = document.querySelector();
+  const commentList = this.document.querySelector();
+
+  submitButton.addEventListener("click", function () {
+    const commentText = commentInput.value();
+    if (commentText === "") {
+      alert("댓글을 입력해주세요.")
+      return;
+    }
+    const newComment = document.createElement("li");
+    newComment.innerHTML = `<div class="comment-item">
+                <div class="comment-author">
+                  <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                  <span>방문자</span>
+                </div>
+                <div class="comment-content">
+                  ${commentText}
+                </div>
+              </div>
+              `;
+    commentList.appendChild(newComment);
+    commentInput.value = "";
+
+  })
+})
